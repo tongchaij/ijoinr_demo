dd67de268706520fbadec140edfe1b7475c5ba16
tongchaij <tongchaij@gmail.com>
2025-01-22 11:39:11 +0700

    Forth

---
+++ wasm/ijoinr_demo_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __wbg_jsowner_free: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_4: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __externref_drop_slice: (a: number, b: number) => void;
+export const __wbindgen_export_7: WebAssembly.Table;
+export const _ZN132__LT_dyn_u20_core__ops__function__FnMut_LT__LP__RP__GT__u2b_Output_u20__u3d__u20_R_u20_as_u20_wasm_bindgen__closure__WasmClosure_GT_8describe6invoke17h0efbeea7d72e0d52E: (a: number, b: number) => void;
+export const closure392_externref_shim: (a: number, b: number, c: any) => void;
+export const closure395_externref_shim: (a: number, b: number, c: any) => void;
+export const closure486_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
