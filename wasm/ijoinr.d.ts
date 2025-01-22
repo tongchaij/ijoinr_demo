f03b4ee76bf5c67ae672ddaeb29cbc7f71ecc2ce
tongchaij <tongchaij@gmail.com>
2025-01-22 11:06:01 +0700

    Second

--- wasm/ijoinr.d.ts
+++ wasm/ijoinr.d.ts
@@ -61,11 +61,11 @@ export interface InitOutput {
   readonly __externref_drop_slice: (a: number, b: number) => void;
   readonly __wbindgen_export_7: WebAssembly.Table;
   readonly _ZN132__LT_dyn_u20_core__ops__function__FnMut_LT__LP__RP__GT__u2b_Output_u20__u3d__u20_R_u20_as_u20_wasm_bindgen__closure__WasmClosure_GT_8describe6invoke17ha7b36f950191379bE: (a: number, b: number) => void;
-  readonly closure362_externref_shim: (a: number, b: number, c: number, d: number, e: any) => void;
-  readonly closure359_externref_shim: (a: number, b: number, c: any) => void;
-  readonly closure365_externref_shim: (a: number, b: number, c: any) => void;
-  readonly closure672_externref_shim: (a: number, b: number, c: any) => void;
-  readonly closure694_externref_shim: (a: number, b: number, c: any, d: any) => void;
+  readonly closure360_externref_shim: (a: number, b: number, c: number, d: number, e: any) => void;
+  readonly closure357_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure363_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure670_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure692_externref_shim: (a: number, b: number, c: any, d: any) => void;
   readonly __wbindgen_start: () => void;
 }
 
